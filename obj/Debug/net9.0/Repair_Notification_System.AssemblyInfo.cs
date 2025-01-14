54c1f2e9f5985c19aea32f476c49417f8bef5ca1
Terapat Wapob <std.65122420209@ubru.ac.th>
2025-01-14 11:00:57 +0700

    test

--- obj/Debug/net9.0/Repair_Notification_System.AssemblyInfo.cs
+++ obj/Debug/net9.0/Repair_Notification_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repair_Notification_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4807e6fdd22784ba767c31fbec1e14d885be54a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repair_Notification_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repair_Notification_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
