4807e6fdd22784ba767c31fbec1e14d885be54a4
Terapat Wapob <std.65122420209@ubru.ac.th>
2025-01-14 10:47:29 +0700

    Create New Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Repair_Notification_System.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
